746e12d9bbfabef1d3f55ddeb4429c1232a45c6b
{name} <{nickstoia1@ou.edu}>
2020-11-09 16:19:26 -0600

    Bites are yummy

--- src/Cake.java
+++ src/Cake.java
@@ -1,4 +1,20 @@
 
 public class Cake {
-
+	private int bites = 10;
+	
+	public Cake() {
+		// nothing to implement
+	}
+	
+	public void takeABite() {
+		--bites;
+		System.out.println("Hmm.. this is tasty!");
+	}
+	
+	public boolean isEmpty() {
+		return bites == 0;
+		// looks like an empty cake
+	}
+	
+	
 }
